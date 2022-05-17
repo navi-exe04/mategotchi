fca42e034714c3611545f2fdc9a2c48058105760
Raul Ivan <ra04navi@outlook.com>
2022-05-17 02:40:23 -0500

    Se hicieron cambios de de diseño en la página pricipal

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,7 @@ const divRegister = document.getElementById('div_registro');
 //Al iniciar la pagina
 window.onload = () => {
     botonesInformacion();
+    sliderAutomatico();
 }
 
 //Eventos para el menu desplegable
@@ -22,6 +23,23 @@ botonMenu.addEventListener('click', (e) => {
 
 /******** FUNCIONES *********/
 
+//Funcion para preguntar sobre el uso de datos del usuario
+
+//Funcion encargada del slider de imagenes
+function sliderAutomatico() {
+
+    //Timer para el Slider de imagenes
+    var counter = 1;
+    setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 5) {
+            counter = 1;    
+        }
+    }, 2500);
+
+}
+
 //Funcion encargada de los eventos de los botones de informacion
 function botonesInformacion() {
 
@@ -32,7 +50,7 @@ function botonesInformacion() {
 
     const botonRegister = document.getElementById("register-btn");
     const botonCerrarRegister = document.getElementById("btn-cerrarRegister");
-    botonRegister.addEventListener('click', abreInfo(divRegister));
+    botonRegister.addEventListener('click', lanzaAlerta());
     botonCerrarRegister.addEventListener('click', abreInfo(divRegister));
 
 }
@@ -52,6 +70,30 @@ function abreInfo(popup) {
         popup.classList.toggle('active');
         allHTML.body.classList.toggle('noScroll');
     };
+}
+
+function lanzaAlerta() {
+    Swal.fire({
+        title: 'Antes de continuar...',
+        text: 'Es muy importante que algún adulto este presente para contestar las siguientes preguntas. Por favor, llama a un adulto para que te ayude.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Continuar',
+        cancelButtonText: 'Más tarde...'
+    }).then((result) => {
+        if(result.isConfirmed) {
+            if (divMenu.classList.contains('show')) {
+                divMenu.classList.remove('show');
+                allHTML.body.classList.remove('noScroll');
+            }
+            if(divLogin.classList.contains('active'))
+                divLogin.classList.toggle('active');
+            divRegister.classList.toggle('active');
+            allHTML.body.classList.toggle('noScroll');
+        }
+    });
 }
 
 //Funcion que se encarga de poner el fondo con filtro blur
